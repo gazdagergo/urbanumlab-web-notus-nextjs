22a46f89d02959263c9b351e7b7a62dc3e878ff7
Gergely Gazda <info@gazdagergo.com>
2022-09-19 15:18:40 +0200

    bush is on fire menu

--- components/Dropdowns/IndexDropdown.js
+++ components/Dropdowns/IndexDropdown.js
@@ -114,6 +114,25 @@ const IndexDropdown = () => {
             WEkEO Notebook
           </a>
         </Link>
+        <div className="h-0 mx-4 my-2 border border-solid border-blueGray-100" />
+        <span
+          className={
+            "text-sm pt-2 pb-0 px-4 font-bold block w-full whitespace-nowrap bg-transparent text-blueGray-400"
+          }
+        >
+          Art
+        </span>
+        <Link href="http://topos.hu/">
+          <a
+            target="_blank"
+            rel="noopener,noreferrer"
+            className={
+              "text-sm py-2 px-4 font-normal block w-full whitespace-nowrap bg-transparent text-blueGray-700"
+            }
+          >
+            Bush is on fire
+          </a>
+        </Link>        
       </div>
     </>
   );
