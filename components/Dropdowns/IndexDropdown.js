893273a16043ee37a1aae42c71f5b2513302c28d
Gergely Gazda <info@gazdagergo.com>
2023-04-19 14:33:04 +0200

    fix menu typo

--- components/Dropdowns/IndexDropdown.js
+++ components/Dropdowns/IndexDropdown.js
@@ -103,7 +103,7 @@ const IndexDropdown = () => {
               "text-sm py-2 px-4 font-normal block w-full whitespace-nowrap bg-transparent text-blueGray-700"
             }
           >
-            VIII. kerületi balesti adatok
+            VIII. kerületi baleseti adatok
           </a>
         </Link>
         <div className="h-0 mx-4 my-2 border border-solid border-blueGray-100" />
